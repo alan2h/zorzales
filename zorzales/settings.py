3dcca44c2e9d1a6e113b02fba52870efebc3349b
Alan Beck <beckalan303@gmail.com>
2019-12-06 17:03:12 -0300

    se sube cambios en el settings, con respecto a la base de datos que se va a usar

--- zorzales/settings.py
+++ zorzales/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'zorzales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'zorzales',
+        'USER': 'sistema',
+        'PASSWORD': 'se242403germanb',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
