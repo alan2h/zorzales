e4497a03bb7fc7a7f85b8d2af7392e05c53d0c67
Alan Beck <beckalan303@gmail.com>
2019-12-20 15:40:10 -0300

    se sube

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django import models
 
-# Create your models here.
+from apps.personas.models import Persona
+
+
+class Cliente(Persona):
+
+    baja = models.BooleanField(default=False)
+    fecha_baja = models.DateField(blank=True, null=True)
+    motivo = models.CharField(max_length=600, blank=True, null=True)
+
+    def __str__(self):
+
+        return '%s, %s' % (self.apellido, self.nombre)
--- apps/personas/models.py
+++ apps/personas/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Persona(models):
 
-   
+    nombre = models.CharField(max_length=3000, blank=False, null=False)
+    apellido = models.CharField(max_length=3000, blank=False, null=False)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+
+
+    def __str__(self):
+
+        return '%s, %s' % (self.apellido, self.nombre)
