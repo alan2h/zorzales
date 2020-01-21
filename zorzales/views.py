49a45a6b707cb3e9139a16670eef3cea5adebc1c
Alan Beck <beckalan303@gmail.com>
2020-01-21 12:13:33 -0300

    se sube cambios nuevos

--- zorzales/views.py
+++ zorzales/views.py
@@ -1,10 +1,17 @@
 
+from django.shortcuts import redirect 
 from django.views.generic import TemplateView
 
 
 class Ingreso(TemplateView):
 
     template_name = 'login.html'
+
+    def post(self, request):
+
+        print(request)
+        print('==========================')
+        return redirect('Dashboard')
 
 
 class Dashboard(TemplateView):
