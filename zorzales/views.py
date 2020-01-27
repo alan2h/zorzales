649b942ace721655771d787ea9843df1c3a4cd05
Alan Beck <beckalan303@gmail.com>
2020-01-27 15:49:57 -0300

    se sube cambios nuevos

--- zorzales/views.py
+++ zorzales/views.py
@@ -1,17 +1,28 @@
 
-from django.shortcuts import redirect 
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+
+# libs login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 
 class Ingreso(TemplateView):
 
     template_name = 'login.html'
 
-    def post(self, request):
 
-        print(request)
-        print('==========================')
-        return redirect('Dashboard')
+    def post(self, request, *args, **kwargs):
+
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST.get('username'), 
+                                 password=request.POST.get('password'))
+            if user is not None:
+                login(request, user)
+                return redirect('/inicio')
+
+        return render(request, 'login.html', {'form': form})
 
 
 class Dashboard(TemplateView):
