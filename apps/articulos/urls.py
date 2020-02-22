dfd46bfb59c3ccd5c93d60f469bdcbc33484497d
Alan <beckalan303@gmail.com>
2020-02-22 19:28:36 -0300

    se sube el eliminar articulos, eliminado de forma fisica

--- apps/articulos/urls.py
+++ apps/articulos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ArticuloCreateView, ArticuloListView
+from .views import ArticuloCreateView, ArticuloListView, \
+    ArticuloUpdateView, ArticuloDeleteView
 
 urlpatterns = [
     path('alta', ArticuloCreateView.as_view(), name='ArticuloCreateView'),
     path('listado', ArticuloListView.as_view(), name='ArticuloListView'),
+    path('editar/<int:pk>', ArticuloUpdateView.as_view(), name='ArticuloUpdateView'),
+    path('eliminar/<int:pk>', ArticuloDeleteView.as_view(), name='ArticuloDeleteView'),
 ]
--- apps/articulos/views.py
+++ apps/articulos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, \
+    UpdateView, DeleteView
 
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -29,5 +30,28 @@ class ArticuloListView(ListView):
             if self.request.GET.get('search') != '':
                 queryset = Articulo.objects.filter(descripcion__icontains=self.request.GET.get('search'))
                 if len(queryset) == 0:
-                    queryset = Articulo.objects.filter(codigo_barra__icontains=self.request.GET.get('search'))
+                    queryset = Articulo.objects.filter(
+                        codigo_barra__icontains=self.request.GET.get('search'))
+                    if len(queryset) == 0:
+                        queryset = Articulo.objects.filter(
+                            marca__descripcion__icontains=self.request.GET.get('search'))
+                        if len(queryset) == 0:
+                            queryset = Articulo.objects.filter(
+                                rubro__descripcion__icontains=self.request.GET.get('search'))
         return queryset
+
+
+class ArticuloUpdateView(SuccessMessageMixin, UpdateView):
+
+    model = Articulo
+    form_class = ArticuloForm
+    template_name = 'articulo_form.html'
+    success_url = '/articulos/listado'
+    success_message = 'El artículo se modifico con éxito'
+
+
+class ArticuloDeleteView(DeleteView):
+
+    model = Articulo
+    template_name = 'articulo_delete_confirm.html'
+    success_url = '/articulos/listado'
