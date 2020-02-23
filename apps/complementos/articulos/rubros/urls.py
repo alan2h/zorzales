60bbc90eee3e179100cd2ea2829da1ccd487386f
Alan <beckalan303@gmail.com>
2020-02-23 12:30:10 -0300

    se sube el formulario de rubros

--- apps/complementos/articulos/rubros/urls.py
+++ apps/complementos/articulos/rubros/urls.py
@@ -1,7 +1,10 @@
-from .views import RubroCreateView, RubroListView
+from .views import RubroCreateView, RubroListView, RubroUpdateView, \
+    RubroDeleteView
 from django.urls import path
 
 urlpatterns = [
     path('alta', RubroCreateView.as_view(), name='RubroCreateView'),
     path('listado', RubroListView.as_view(), name='RubroListView'),
+    path('editar/<int:pk>', RubroUpdateView.as_view(), name='RubroUpdateView'),
+    path('eliminar/<int:pk>', RubroDeleteView.as_view(), name='RubroDeleteView'),
 ]
--- apps/complementos/articulos/rubros/views.py
+++ apps/complementos/articulos/rubros/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, \
+    DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Rubro
@@ -19,3 +20,29 @@ class RubroListView(ListView):
 
     queryset = Rubro.objects.all()
     template_name = 'rubros_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+
+        queryset = Rubro.objects.all()
+        if 'search' in self.request.GET:
+            if self.request.GET.get('search') != '':
+                queryset = Rubro.objects.filter(descripcion__icontains=\
+                    self.request.GET.get('search'))
+        return queryset
+
+
+class RubroUpdateView(SuccessMessageMixin, UpdateView):
+
+    model = Rubro
+    form_class = RubroForm
+    template_name = 'rubro_form.html'
+    success_url = '/complementos/rubros/listado'
+    success_message = 'El rubro se agrego con éxito'
+
+
+class RubroDeleteView(DeleteView):
+
+    model = Rubro
+    template_name = 'rubro_delete_confirm.html'
+    success_url = '/complementos/rubros/listado'
