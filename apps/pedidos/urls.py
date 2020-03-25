a21166899bc53947d8003e9bf2a4e3ddbde01d71
Alan <beckalan303@gmail.com>
2020-03-25 01:11:45 -0300

    se sube camiobs nuevos

--- apps/pedidos/urls.py
+++ apps/pedidos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PedidoCreateView
+from .views import PedidoCreateView, PedidoListView
 
 from . import helpers
 from . import viewsets
@@ -13,6 +13,7 @@ router.register(r'api', viewsets.PedidoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('alta', PedidoCreateView.as_view(), name='PedidoCreateView'),
+    path('listado', PedidoListView.as_view(), name='PedidoListView'),
     path('articulos/listado', helpers.articulo_list, name='articulo_list'),
     path('articulos/obtener', helpers.get_articulo, name='get_articulo'),
 ]
