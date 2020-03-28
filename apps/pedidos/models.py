013d09601dbac57675aa68eaa5b0df31cdc5ebf4
Alan <beckalan303@gmail.com>
2020-03-28 20:45:23 -0300

    se sube pedidos como tiene que ser

--- apps/pedidos/models.py
+++ apps/pedidos/models.py
@@ -3,14 +3,29 @@ from django.db import models
 from apps.articulos.models import Articulo
 
 
+class PedidoArticulo(models.Model):
+
+    articulo = models.ForeignKey(Articulo, related_name='articulos', blank=False,
+    on_delete=models.CASCADE)
+    cantidad = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return '%s - %s' % (str(self.cantidad), str(self.articulo.descripcion))
+
+    class Meta:
+        verbose_name = 'Pedido-Articulo'
+        verbose_name_plural = 'Pedidos-Articulos'
+
+
 class Pedido(models.Model):
 
     fecha = models.DateField(null=False, blank=False)
-    articulos = models.ManyToManyField(Articulo, related_name='articulos_pedidos', blank=False)
     precio_compra = models.DecimalField(decimal_places=2, max_digits=12, 
     null=True, blank=True)
     observacion = models.TextField(max_length=300, null=True, blank=True)
 
+    pedido_articulo = models.ManyToManyField(PedidoArticulo, related_name='articulos_pedidos', 
+    blank=False)
     # para realizar bajas
 
     baja = models.BooleanField(default=False, null=True, blank=True)
@@ -23,3 +38,4 @@ class Pedido(models.Model):
     class Meta:
         verbose_name = 'Pedido'
         verbose_name_plural = 'Pedidos'
+
